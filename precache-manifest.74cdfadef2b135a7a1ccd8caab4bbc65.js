3cd780bbf95b69ece63c9ecdf44e6834a35c3a40
aditraanggra <aditra.anggra@gmail.com>
2020-03-25 17:09:46 +0700

    Initial

---
+++ precache-manifest.74cdfadef2b135a7a1ccd8caab4bbc65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f69fcfd89ffefff0ffc",
+    "url": "/static/css/main.d4e17e67.chunk.css"
+  },
+  {
+    "revision": "1f69fcfd89ffefff0ffc",
+    "url": "/static/js/main.1f69fcfd.chunk.js"
+  },
+  {
+    "revision": "4f0ef6b5e877cad86b0a",
+    "url": "/static/js/1.4f0ef6b5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b84d7f45fa0b40995b092b343183d448",
+    "url": "/index.html"
+  }
+];
